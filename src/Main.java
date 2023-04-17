95caf8284d320656bc84a141f30394e2983ae23c
3enani <afa.enani@hotmail.com>
2023-04-16 23:39:30 -0400

    added lock instruction

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import ca.concordia.coen346.server.Buffer;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Buffer buffer = new Buffer();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                buffer.lock.lock();
+                try {
+                    int pos = buffer.getNextPosition();
+                    buffer.insertItem(i, pos);
+                    buffer.setIn((pos + 1) % Buffer.BUFFER_SIZE);
+                    buffer.readCount();
+                }
+                finally {
+                    buffer.lock.unlock();
+                }
+            }
+        });
+        Thread t2 = new Thread(() -> {
+           for (int i = 0; i < 1000; i++) {
+               buffer.lock.lock();
+               try {
+                   int pos = (buffer.getNextPosition() - 1 + Buffer.BUFFER_SIZE) % Buffer.BUFFER_SIZE;
+                   buffer.getNextItem(pos);
+                   buffer.readCount();
+               }
+               finally {
+                   buffer.lock.unlock();
+               }
+           }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count " + buffer.readCount());
     }
 }
--- src/ca/concordia/coen346/server/Buffer.java
+++ src/ca/concordia/coen346/server/Buffer.java
@@ -1,36 +1,70 @@
 package ca.concordia.coen346.server;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Buffer {
-    private final static int BUFFER_SIZE = 10;
+    public final static int BUFFER_SIZE = 10;
 
     private int[] buffer = new int[BUFFER_SIZE];
     private int in;
     private int out;
     private int count;
+    public Lock lock;
 
 
     public Buffer(){
+        this.lock = new ReentrantLock(); // create lock
     }
 
     public int readCount(){
-        return count;
+        lock.lock(); //acquire lock
+        try {
+            return count;
+        }
+        finally {
+            lock.unlock(); //release lock
+        }
     }
 
     public int getNextItem(int pos){
-        int item = buffer[pos];
-        return item;
+        lock.lock();
+        try {
+            int item = buffer[pos];
+            return item;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     public void setIn(int pos){
-        this.in = pos;
+        lock.lock();
+        try {
+            this.in = pos;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     public int getNextPosition(){
-        return in;
+        lock.lock();
+        try {
+            return in;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
     public void insertItem(int item, int pos){
-        buffer[pos] = item;
+        lock.lock();
+        try {
+            buffer[pos] = item;
+        }
+        finally {
+            lock.unlock();
+        }
     }
 
 }
